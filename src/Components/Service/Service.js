791b414863e271746e8a412263980f4077c4d0ff
Md. Sumon Mia <md.sumonmia.cse.iu@gmail.com>
2022-04-17 04:15:14 +0600

    services added

--- src/Components/Service/Service.js
+++ src/Components/Service/Service.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const Service = () => {
+const Service = ({ service }) => {
+    const { name, img, price, comment } = service;
     return (
         <div>
-            
+
+            <div className="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+
+                <img className="rounded-t-lg p-3" src={img} alt="" />
+
+                <div className="p-5 text-center">
+
+                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{name}</h5>
+
+                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400" title={comment}>{comment.length > 200 ? comment.slice(0, 200) +'...Read More' : comment}</p>
+
+                    <Link to="/login" className="inline-flex items-center py-3 px-20 text-sm font-medium text-center text-white focus:outline-none bg-[#213162] rounded-full border border-gray-200 hover:bg-blue-500  focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
+                        Book Now
+                        <svg className="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                    </Link>
+                </div>
+            </div>
+
         </div>
     );
 };
--- src/Components/Services/Services.js
+++ src/Components/Services/Services.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import useService from '../../hooks/useService';
 import icon from '../../images/logo/title-icon-1.png'
+import Service from '../Service/Service';
 const Services = () => {
+    const [services] = useService()
     return (
-        <div className='bg-[#E9F2EB] h-72'>
-            <div className='flex flex-col justify-center items-center pt-20'>
+        <div className='bg-[#E9F2EB] py-10'>
+            <div className='flex flex-col justify-center items-center pt-10'>
                 <p className='text-2xl font-mono font-bold'>Doctor Services</p>
-                <img  src={icon} alt="" />
+                <img src={icon} alt="" />
 
             </div>
+             <div className='grid md:grid-cols-3 justify-center items-center w-[80%] mx-auto gap-5 my-10'>
+                 {
+                     services.map(service=><Service
+                     service={service}
+                     key={service.id}
+                     ></Service>)
+                 }
+             </div>
 
         </div>
     );
