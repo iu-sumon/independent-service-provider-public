d749226414fc339b65f0fa7ff71a116d8d8b54bc
Md. Sumon Mia <md.sumonmia.cse.iu@gmail.com>
2022-04-18 21:30:17 +0600

    finally added

--- src/Components/CheckOut/CheckOut.js
+++ src/Components/CheckOut/CheckOut.js
@@ -8,7 +8,7 @@ const CheckOut = () => {
     const [name, setName] = useState('')
     const [email, setEmail] = useState('')
     const [number, setNumber] = useState('')
-    const [address, setAddress]=useState('')
+    const [address, setAddress] = useState('')
     const [description, setDescription] = useState('')
     const [updateProfile, updating, error] = useUpdateProfile(auth);
     let getError;
@@ -29,7 +29,7 @@ const CheckOut = () => {
                     <h5 className="text-xl font-medium text-gray-900 dark:text-white text-center">Patient Information</h5>
                     <div>
                         <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Name</label>
-                        <input onBlur={(e) => setName(e.target.value)} type="name" name="name" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="Enter your name" required/>
+                        <input onBlur={(e) => setName(e.target.value)} type="name" name="name" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="Enter your name" required />
                     </div>
                     <div>
                         <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Email</label>
@@ -37,11 +37,11 @@ const CheckOut = () => {
                     </div>
                     <div>
                         <label htmlFor="number" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Phone Number</label>
-                        <input onBlur={(e) => setNumber(e.target.value)} type="number" name="number" id="number" placeholder="Enter your phone number" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required/>
+                        <input onBlur={(e) => setNumber(e.target.value)} type="number" name="number" id="number" placeholder="Enter your phone number" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required />
                     </div>
                     <div>
                         <label htmlFor="address" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Address</label>
-                        <input onBlur={(e) => setAddress(e.target.value)} type="text" name="address" id="address" placeholder="Enter your address" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required/>
+                        <input onBlur={(e) => setAddress(e.target.value)} type="text" name="address" id="address" placeholder="Enter your address" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required />
                     </div>
                     <div>
                         <label htmlFor="description" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Description</label>
@@ -51,7 +51,7 @@ const CheckOut = () => {
                     <button
 
                         onClick={async () => {
-                            await updateProfile({ name, email, number,address, description });
+                            await updateProfile({ name, email, number, address, description });
                             toast('Thanks for booking and Provide your information');
                         }}
 
